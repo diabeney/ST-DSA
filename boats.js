b319d54a844ede31689af3300af3c7a62073427a
Diabene <addodiabene69@gmail.com>
2023-04-05 00:28:40 +0000

    dsa

--- boats.js
+++ boats.js
@@ -1,14 +1,26 @@
-const people = [3, 2,2,1];
+const people = [3,2,2,1];
 const limit = 3;
 
 function boat(people, limit) {
     let numberOfBoats = 0;
-    people.reduce((prev,curr) => {
-        if(prev + curr <= limit) {
-            numberOfBoats++
-        }
-    })
-    return numberOfBoats;
+    for(let i = 0;i<people.length;i++) {
+        let curr = people[i]; 
+        let pointer = i + 1;
+        if(curr === limit) {
+            numberOfBoats++;
+            continue;
+        } else if(curr < limit){
+            numberOfBoats++;
+            while(pointer < people.length) {
+                console.log(pointer)
+                let next = people[pointer];
+                if(next + curr <= limit) numberOfBoats++;
+                pointer++
+            }
+        } else continue;
+    }
+    // return numberOfBoats;
 }
-
-console.log(boat(people, limit))
+console.time('dsa');
+console.log(boat(people, limit));
+console.timeEnd('dsa')
